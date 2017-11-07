dd78f6b4970131f949e72ecee3f21e80a367ff1b
nelsonemilio <rodrigueznelsonemilio@gmail.com>
2017-11-07 16:10:09 -0300

    Diagramas de clase

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/carrito_compra/';
+$config['base_url'] = 'http://localhost/carrito_optica/';
 
 /*
 |--------------------------------------------------------------------------
